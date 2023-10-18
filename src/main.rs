537d98828848985939bf9752643e994e4dfa4d42
Federico Squartini <federico.squartini@gmail.com>
2023-10-18 20:21:45 +0200

    feat: parse the seed argument on the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Command, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Great Wall")
+    .version("0.0.1")
+    .author("Federico Squartini <squarfed@bigfi.re>")
+    .about("Great Wall bip39 fractal store")
+    .arg(
+        Arg::new("seed")
+            .num_args(1)
+            .value_name("SEED")
+            .help("Input seed")
+            .required(true)
+    ).get_matches();
+
+println!("{:#?}", matches);
+
 }
