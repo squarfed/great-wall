2eb25d98f6a61357660c225deb08fd83b5b001c9
Federico Squartini <federico.squartini@gmail.com>
2023-10-24 06:21:32 +0200

    feat: hash seed using Argon2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use clap::{Parser};
+use argon2::{Argon2, Error};
+use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// Wallet seed value
     seed: String,
-
 }
 
-
-fn main() {
+fn main() -> Result<(), Error> {
     let cli = Cli::parse();
 
-    if let seed = cli.seed {
-        println!("Value for seed: {seed}");
-    }
+    let seed = cli.seed;
+    println!("Value for seed: {seed}");
+
+    let password: &[u8] = seed.as_bytes();
+    let salt: &[u8; 12] = b"example salt"; // Todo: ?
+
+    let mut output_key_material: [u8; 32] = [0u8; 32]; // Can be any desired size
+    Argon2::default().hash_password_into(password, salt, &mut output_key_material)?;
+    println!("{:#?}", output_key_material);
+    Ok(())
 }
