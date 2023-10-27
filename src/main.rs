d79960c97584ef9c77def5818f0f9ecc8c6fd98a
Federico Squartini <federico.squartini@gmail.com>
2023-10-27 18:39:45 +0200

    feat: explicitly set argon2 iterations number aka time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use argon2::Argon2;
+use argon2::{Argon2, ParamsBuilder};
 use clap::Parser;
 use hotwatch::{Event, EventKind, Hotwatch};
 use std::process::Command;
@@ -16,13 +16,12 @@ struct Cli {
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-
+    let iteration_number = 50;
     let seed = cli.seed;
     println!("Value for seed: {seed}");
 
     let password: &[u8] = seed.as_bytes();
     let salt: &[u8; 12] = b"example salt"; // Todo: ?
-
     let mut output_key_material: [u8; 32] = [0u8; 32]; // Can be any desired size
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(Duration::from_millis(120));
@@ -40,7 +39,11 @@ fn main() -> Result<()> {
             ]),
     );
     pb.set_message("hashing...");
-    Argon2::default()
+    let params = ParamsBuilder::new()
+        .t_cost(iteration_number)
+        .build()
+        .unwrap();
+    Argon2::from(params)
         .hash_password_into(password, salt, &mut output_key_material)
         .expect("Failed to hash passord");
     pb.finish_with_message("done");
